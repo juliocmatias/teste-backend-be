e010fb8eb9e3125d8df2f2f891330f467f075ceb
Julio Matias <juliocesark2@hotmail.com>
2024-06-26 16:32:53 -0300

    feat: implement migration of sequelize by users

---
+++ src/database/migrations/01-create-users-table.ts
@@ -0,0 +1,27 @@
+import { Model, QueryInterface, DataTypes } from 'sequelize';
+import IUser from '../../interfaces/users/IUser';
+
+export default {
+  up(queryInterface: QueryInterface) {
+    return queryInterface.createTable<Model<IUser>>('users', {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  },
+
+  down(queryInterface: QueryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
