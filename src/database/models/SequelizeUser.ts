26fe2a53733e74f87853a68b78d68b02999efac3
Julio Matias <juliocesark2@hotmail.com>
2024-06-27 23:56:00 -0300

    feat: implement SequelizeUser model

---
+++ src/database/models/SequelizeUser.ts
@@ -0,0 +1,39 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeUser extends Model<InferAttributes<SequelizeUser>,
+  InferCreationAttributes<SequelizeUser>> {
+  declare id: CreationOptional<number>;
+
+  declare email: string;
+  declare password: string;
+}
+
+SequelizeUser.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  }
+}, {
+  sequelize: db,
+  modelName: 'users',
+  timestamps: false,
+});
+
+export default SequelizeUser;
